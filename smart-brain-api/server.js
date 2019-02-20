d14e8a611aa52a479e6eb28f279e470f2b00e281
Julia Martin <38506447+jmar90@users.noreply.github.com>
2019-02-20 11:20:53 -0500

    Added sign in and register routes

--- smart-brain-api/server.js
+++ smart-brain-api/server.js
@@ -1,10 +1,64 @@
-// EXPRESS SET UP
+// EXPRESS AND BODY-PARSER SET UP
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const app = express(); 
 
+app.use(bodyParser.json());
+
+// TEMP DATA (use until database is set up)
+const database = {
+	users: [
+		{
+			id: '123',
+			name: 'John',
+			email: 'john@gmail.com',
+			password: 'cookies',
+			entries: 0,
+			joined: new Date()
+		},
+		{
+			id: '124',
+			name: 'Sally',
+			email: 'sally@gmail.com',
+			password: 'bananas',
+			entries: 0,
+			joined: new Date()
+		}
+	]
+}
+
 // ROUTES
+/* Root/home route */
 app.get('/', (req, res) => {
-	res.send('This is working');
+	res.send(database.users);
+})
+
+/* Sign In */
+app.post('/signin', (req, res) => {
+	if(req.body.email === database.users[0].email &&
+		req.body.password === database.users[0].password) {
+		res.json('Success');
+	} else {
+		res.status(400).json('Error logging in');
+	}
+})
+
+/* Register (create new user) */
+app.post('/register', (req, res) => {
+	// Grab email, name, & password from the request.body using destructuring
+	const { email, name, password } = req.body;
+	// For time being, push data into users array (eventually, will create database)
+	database.users.push({
+		id: '125',
+		name: name,
+		email: email,
+		password: password,
+		entries: 0,
+		joined: new Date()
+	})
+	// Return last item in users array (ie, the data for the user that was just created)
+	res.json(database.users[database.users.length-1]);
 })
 
 // CREATE SERVER ON PORT 3000
@@ -20,5 +74,4 @@ Routes:
 /register --> POST request (add data to database). Respond with new user object.
 /profile/:userId --> GET request (get user info). Respond with user.
 /image --> PUT (will update score). Respond with updated user object.
-
 */
